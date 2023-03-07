11d65d3b8f777e87fc980a7cf2e5b08b9e566abd
Krishna Tandon <tandonkrishna1208@gmail.com>
2023-03-07 22:56:40 +0530

    Intro to Python

---
+++ HelloWorld.py
@@ -0,0 +1,24 @@
+print("Hello Python World!")
+print("100 + 50")
+print("My name is \"Krishna\"")
+
+print("This is first line and \n this is 2nd line \n and 3rd line")
+
+
+print('This isn\'t a number')
+
+print('This sign => \\ is backslash')
+
+print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+print("Backslash (\\)")
+print("Forward Slash (/)")
+
+#firstName = input("Enter first name : ")
+#lastName = input("Enter last name : ")
+#print(firstName, lastName)
+
+beerFirstName = input("Enter beer first name : ")
+beerLastName = input("Enter beer last name :")
+
+print("Your beer brand name\n",f"\"{beerFirstName} {beerLastName}\"\nCongratulations!!")
